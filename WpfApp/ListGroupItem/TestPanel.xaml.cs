6d0cf0ea283efca7910013ecbf689d58e1ead6d8
Zjw <762259059@qq.com>
2021-02-02 19:51:52 +0800

    init

---
+++ WpfApp/ListGroupItem/TestPanel.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListGroupItem
+{
+    /// <summary>
+    /// TestPanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class TestPanel : UserControl
+    {
+        public TestPanel()
+        {
+            InitializeComponent();
+        }
+
+        private IDIO io;
+        public IDIO IO
+        {
+            set
+            {
+                io = value;
+                IOInit();
+            }
+            get => io;
+        }
+
+        private void IOInit()
+        {
+            if (IO != null)
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    //设置列表控件资源，并定义组的规则和排序
+                    its.ItemsSource = IO;
+                    ICollectionView cv = CollectionViewSource.GetDefaultView(its.ItemsSource);
+                    cv.SortDescriptions.Clear();
+                    cv.GroupDescriptions.Clear();
+                    cv.SortDescriptions.Add(new SortDescription("Attr.IOIndex", ListSortDirection.Ascending));
+                    cv.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+                    cv.GroupDescriptions.Add(new PropertyGroupDescription("Attr.GroupName"));
+                }, System.Windows.Threading.DispatcherPriority.Send);
+            }
+        }
+    }
+
+    public class IDIO : IEnumerable<IoItem>
+    {
+        public IEnumerator<IoItem> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IoItem
+    {
+
+    }
+}
---
+++ WpfApp/WpfApp/EventAggregratorPanel.xaml.cs
@@ -0,0 +1,64 @@
+﻿using CommonEventAggregator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// EventAggregratorPanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class EventAggregratorPanel : UserControl, IEventHandler<TestEventArgs>
+    {
+        public EventAggregratorPanel()
+        {
+            InitializeComponent();
+
+            CMEventAggregator.Instance.Subscribe(this);
+            
+        }
+
+        public void Handler(TestEventArgs @event)
+        {
+            ls_box.Items.Add(@event.ObjVal);
+            ls_box.IsEnabled = true;
+            ls_box.Opacity = 1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CMEventAggregator.Instance.Publish(new TestEventArgs("btn click"));
+        }
+    }
+
+    public class TestEventArgs : IEventArgs
+    {
+        private object objVal;
+        public object ObjVal
+        {
+            get
+            {
+                return objVal;
+            }
+            set
+            {
+                objVal = value;
+            }
+        }
+        public TestEventArgs(object val)
+        {
+            ObjVal = val;
+        }
+    }
+}
